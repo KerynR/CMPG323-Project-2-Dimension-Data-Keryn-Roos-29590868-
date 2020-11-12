3e168e5ffb7c4639062d60bbb244760afbf9592d
KerynR <61046556+KerynR@users.noreply.github.com>
2020-11-12 19:43:17 +0200

    inital commit

---
+++ KRoos_29590868_Project2_DimensionData/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KRoos_29590868_Project2_DimensionData.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ KRoos_29590868_Project2_DimensionData/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KRoos_29590868_Project2_DimensionData.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
