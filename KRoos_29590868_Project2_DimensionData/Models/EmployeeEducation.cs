167b3e3bd7e081084a955a6271cbc33f29f0cab7
KerynR <61046556+KerynR@users.noreply.github.com>
2020-11-16 15:36:01 +0200

    Fix models

---
+++ KRoos_29590868_Project2_DimensionData/Models/EmployeeEducation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KRoos_29590868_Project2_DimensionData.Models
+{
+    public class EmployeeEducation
+    {
+    }
+}
